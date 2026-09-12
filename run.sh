97eb93ae9ec240b1c98a73ea37664abbc45dc131
Daniela Puiu <dpuiu@jhu.edu>
2026-09-12 11:35:53 -0400

    app update

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-git init
-gh repo create pwatest --public --source=. --remote=origin --push
+#git init
+#git add .
+#git commit -m 'initial commit'
+#gh repo create pwatest --public --source=. --remote=origin --push
+
+#####
+git add .
+git commit -m 'app update'
+git push
